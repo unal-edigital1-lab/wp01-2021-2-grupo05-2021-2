17dde82998e4e7194e2c5ce100face2c656db003
nicolasv30 <nivelasquezo@unal.edu.co>
2022-01-30 16:31:04 -0400

    .

--- hdl/quartus/scr/VGA_driver.v
+++ hdl/quartus/scr/VGA_driver.v
@@ -1,7 +1,7 @@
 
 module VGA_Driver640x480 (
-	input rst,
-	input clk, 				// 25MHz  para 60 hz de 640x480
+	input rst,  // Entrada reset
+	input clk, 	// Entrada reloj 25MHz para 60 hz de 640x480
 	input  [11:0] pixelIn, 	// entrada del valor de color  pixel 
 	output  [11:0] pixelOut, // salida pixel VGA (RGB)
 	output  Hsync_n,		// seÃ±al de sincronizaciÃ³n en horizontal negada
@@ -10,15 +10,15 @@ module VGA_Driver640x480 (
 	output  [8:0] posY 		// posicion en vertical  del pixel siguiente
 );
 
-localparam SCREEN_X = 640; 	// tamaño de la pantalla visible en horizontal 
-localparam FRONT_PORCH_X =16;  
+localparam SCREEN_X = 640; // Tamaño de la VGA horizontal 
+localparam FRONT_PORCH_X =16;  // Pixeles de sincronización
 localparam SYNC_PULSE_X = 96;
 localparam BACK_PORCH_X = 48;
 localparam TOTAL_SCREEN_X = SCREEN_X+FRONT_PORCH_X+SYNC_PULSE_X+BACK_PORCH_X; 	// total pixel pantalla en horizontal 
 
 
-localparam SCREEN_Y = 480; 	// tamaño de la pantalla visible en Vertical 
-localparam FRONT_PORCH_Y =10;  
+localparam SCREEN_Y = 480;  // Tamaño de la VGA vertical
+localparam FRONT_PORCH_Y =10;  // Pixeles de sincronización
 localparam SYNC_PULSE_Y = 2;
 localparam BACK_PORCH_Y = 33;
 localparam TOTAL_SCREEN_Y = SCREEN_Y+FRONT_PORCH_Y+SYNC_PULSE_Y+BACK_PORCH_Y; 	// total pixel pantalla en Vertical 
@@ -30,10 +30,10 @@ reg  [8:0] countY;
 assign posX    = countX;
 assign posY    = countY;
 
-assign pixelOut = (countX<SCREEN_X) ? (pixelIn ) : (12'b000000000000) ;
+assign pixelOut = (countX<SCREEN_X) ? (pixelIn ) : (12'b000000000000) ; // Dejar en blanco la zona del porch
 
-assign Hsync_n = ~((countX>=SCREEN_X+FRONT_PORCH_X) && (countX<SCREEN_X+SYNC_PULSE_X+FRONT_PORCH_X)); 
-assign Vsync_n = ~((countY>=SCREEN_Y+FRONT_PORCH_Y) && (countY<SCREEN_Y+FRONT_PORCH_Y+SYNC_PULSE_Y));
+assign Hsync_n = ~((countX>=SCREEN_X+FRONT_PORCH_X) && (countX<SCREEN_X+SYNC_PULSE_X+FRONT_PORCH_X)); // Sincronización horizontal
+assign Vsync_n = ~((countY>=SCREEN_Y+FRONT_PORCH_Y) && (countY<SCREEN_Y+FRONT_PORCH_Y+SYNC_PULSE_Y)); // Sincronización vertical
 
 
 always @(posedge clk) begin
